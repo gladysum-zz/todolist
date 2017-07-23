adccbbd59d352d613b2de56b7f3652ac4bccec38
Gladys Um <gladysum@gmail.com>
2017-07-23 15:53:20 -0400

    added enzyme

--- browser/About.test.js
+++ browser/About.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import About from './About';
 
 describe('About', () => {
@@ -7,4 +8,10 @@ describe('About', () => {
         const div = document.createElement('div');
         ReactDOM.render(<About/>, div);
     });
+
+    it('renders the correct text', () => {
+      const about = shallow(<About/>);
+      expect(about.text()).toEqual('This is the About Page.');
+    })
+
 });
