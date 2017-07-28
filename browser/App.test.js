d26472c80c846cf4e019e559156b3a598d647eff
Gladys Um <gladysum@admins-mbp.lan>
2017-07-28 18:32:56 -0400

    modifed App test

--- browser/App.test.js
+++ browser/App.test.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
 
+function createFakeStore (state) {
+	return {
+		dispatch: () => {},
+		subscribe: () => {},
+		getState: () => {
+			return Object.assign({}, state);
+		}
+	}
+}
+
+function setup (props={}, state={}) {
+	const fakeStore = createFakeStore(state);
+
+	return mount(<App {...props} store={fakeStore} />);
+}
+
 describe('App', () => {
 
 	it('renders without crashing', () => {
-		const div = document.createElement('div');
-		ReactDOM.render(
-			<Provider store={store}>
-				<App/>
-			</Provider>	 
-		,div);
+		const fakeProps = {};
+		const wrapper = setup(fakeProps, {});
+
+		expect(wrapper.find(App).props('tasks')).toHaveLength(1);
+
+
+
+		// const div = document.createElement('div');
+		// ReactDOM.render(
+		// 	<Provider store={store}>
+		// 		<App/>
+		// 	</Provider>	 
+		// ,div);
 	});
 
 });
